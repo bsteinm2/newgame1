bdf99d35a63e32dcd2a14c67f005ca053494edda
brandon-steinman <bsteinm2@illinois.edu>
2021-02-21 13:24:20 -0600

    window creation failing

--- include/startup_utils.h
+++ include/startup_utils.h
@@ -7,9 +7,17 @@ SDL_Window * window;
 SDL_Surface * window_surface;
 SDL_DisplayMode DM;
 //SDL_DisplayMode DM;
-	
+
+typedef struct window_data
+{
+SDL_Rect bounds;
+SDL_Window * window;
+} window_data;	
+
 int get_screen_width();
 int get_screen_height();
 int initSDL();
+
+
 
 #endif
--- startup_utils.c
+++ startup_utils.c
@@ -1,3 +1,7 @@
+/* ASSUMPTIONS:
+    - the display that is returned first from SDL_GetDisplayBounds is the one to use
+*/
+
 #include "include/startup_utils.h"
 
 #include <stdio.h>
@@ -5,17 +9,28 @@
 
 //SDL_DisplayMode DM;
 
+
 int initSDL() {
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         SDL_Log("SDL couldnt init. Error: : %s\n", SDL_GetError());
         return -1;
     } else {
+        // int numdisplays = SDL_GetNumVideoDisplays();
+        window_data w;
+        // for (int i = 0; i < numdisplays; i++) {
+        //     SDL_GetDisplayBounds( i, &w.bounds );
+        //     printf("%d, %d, %d, %d\n", w.bounds.h, w.bounds.w, w.bounds.x, w.bounds.y);
+        // }
         // make the WINDOW
         int width = get_screen_width();
         int height = get_screen_height();
-        // printf("\n width: %d\n", width);
-        window = SDL_CreateWindow( "untitled0", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN );
+        SDL_GetDisplayBounds( 0, &w.bounds );
+        printf("%d, %d, %d, %d\n", w.bounds.h, w.bounds.w, w.bounds.x, w.bounds.y);
+        printf("\n height: %d\n", height);
+        printf("\n width: %d\n", width);
+        //window = SDL_CreateWindow( "untitled0", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_SHOWN );
+        SDL_CreateWindow("untitled", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, w.bounds.w, w.bounds.h, SDL_WINDOW_SHOWN);
 		if( window == NULL ) {
             printf("sdl window not created");
             return -1;
